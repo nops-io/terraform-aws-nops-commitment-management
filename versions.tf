03768a605d8c34213239fda8a9f039ca850bf91d
werner-nops <werner@nops.io>
2024-11-22 10:40:07 -0300

    fix: Removing unnecessary dependencies (#5)
    * fix: fixing workflows and adding region autodetection
    
    * test: testing deploying module on payer and child account
    
    * fix: Removing unnecessary dependencies
    
    ---------
    
    Co-authored-by: Werner Matias Kahnlein <matias.kahnlein@caylent.com>

--- versions.tf
+++ versions.tf
@@ -5,17 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.7"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.3"
-    }
   }
 }
